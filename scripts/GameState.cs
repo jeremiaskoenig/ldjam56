00283b2caaf31ba48e0304346cee3e1f8b123f85
Lollister <lorenzsass@web.de>
2024-10-07 23:20:23 +0200

    World Building Part X: Citys

--- scripts/GameState.cs
+++ scripts/GameState.cs
@@ -3,97 +3,97 @@ using System;
 
 public partial class GameState : Node3D
 {
-    public static event Action Initialized;
+	public static event Action Initialized;
 
-    public static GameState Instance { get; private set; }
+	public static GameState Instance { get; private set; }
 
-    public int Score { get; private set; }
-    public int HighScore { get; private set; }
+	public int Score { get; private set; }
+	public int HighScore { get; private set; }
 
-    public event Action<int> ScoreChanged;
-    public event Action<int> GameOver;
+	public event Action<int> ScoreChanged;
+	public event Action<int> GameOver;
 
-    public override void _Ready()
-    {
-        if (Instance == null)
-            Instance = this;
-        else
-            GD.PrintErr($"Duplicate GameState. Source: {Name}");
+	public override void _Ready()
+	{
+		if (Instance == null)
+			Instance = this;
+		else
+			GD.PrintErr($"Duplicate GameState. Source: {Name}");
 
-        LoadHighScore();
+		LoadHighScore();
 
-        Initialized?.Invoke();
-    }
+		Initialized?.Invoke();
+	}
 
-    private void LoadHighScore()
-    {
-        if (FileAccess.FileExists("user://highscore.dat"))
-        {
-            using var file = FileAccess.Open("user://highscore.dat", FileAccess.ModeFlags.Read);
-            HighScore = (int)file.Get32();
-        }
-    }
+	private void LoadHighScore()
+	{
+		if (FileAccess.FileExists("user://highscore.dat"))
+		{
+			using var file = FileAccess.Open("user://highscore.dat", FileAccess.ModeFlags.Read);
+			HighScore = (int)file.Get32();
+		}
+	}
 
-    [Export]
-    public Node3D GameContainer { get; set; }
+	[Export]
+	public Node3D GameContainer { get; set; }
 
-    [Export]
-    public int RoundLength { get; set; } = 300;
+	[Export]
+	public int RoundLength { get; set; } = 300;
 
-    [Export]
-    public PackedScene Level { get; set; }
-    private Node3D currentLevel;
+	[Export]
+	public PackedScene Level { get; set; }
+	private Node3D currentLevel;
 
-    [Export]
-    public Vector3 PlayerSpawn { get; set; }
+	[Export]
+	public Vector3 PlayerSpawn { get; set; }
 
-    public double CurrentRoundTimer { get; set; }
-    public bool IsRoundRunning { get; set; }
+	public double CurrentRoundTimer { get; set; }
+	public bool IsRoundRunning { get; set; }
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (IsRoundRunning)
-            CurrentRoundTimer -= delta;
+	public override void _PhysicsProcess(double delta)
+	{
+		if (IsRoundRunning)
+			CurrentRoundTimer -= delta;
 
-        if (CurrentRoundTimer < 0)
-        {
-            IsRoundRunning = false;
-            ProcessMode = ProcessModeEnum.Disabled;
+		if (CurrentRoundTimer < 0)
+		{
+			IsRoundRunning = false;
+			ProcessMode = ProcessModeEnum.Disabled;
 
-            if (Score > HighScore)
-            {
-                HighScore = Score;
+			if (Score > HighScore)
+			{
+				HighScore = Score;
 
-                using var file = FileAccess.Open("user://highscore.dat", FileAccess.ModeFlags.Write);
-                if (file == null)
-                {
-                    GD.PrintErr(FileAccess.GetOpenError());
-                }
-                else
-                    file.Store32((uint)Score);
-            }
+				using var file = FileAccess.Open("user://highscore.dat", FileAccess.ModeFlags.Write);
+				if (file == null)
+				{
+					GD.PrintErr(FileAccess.GetOpenError());
+				}
+				else
+					file.Store32((uint)Score);
+			}
 
-            GameOver?.Invoke(Score);
-        }
-    }
+			GameOver?.Invoke(Score);
+		}
+	}
 
-    public void RestartGame()
-    {
-        currentLevel?.Free();
-        currentLevel = Level.Instantiate<Node3D>();
-        GameContainer.AddChild(currentLevel);
-        Score = 0;
+	public void RestartGame()
+	{
+		currentLevel?.Free();
+		currentLevel = Level.Instantiate<Node3D>();
+		GameContainer.AddChild(currentLevel);
+		Score = 0;
 
-        (GameContainer.FindChild("Player") as Node3D).Position = PlayerSpawn;
+		(GameContainer.FindChild("Player") as Node3D).Position = PlayerSpawn;
 
-        CurrentRoundTimer = RoundLength;
-        IsRoundRunning = true;
-        ProcessMode = ProcessModeEnum.Inherit;
-    }
+		CurrentRoundTimer = RoundLength;
+		IsRoundRunning = true;
+		ProcessMode = ProcessModeEnum.Inherit;
+	}
 
-    public void AddScore(int value)
-    {
-        Score += value;
-        ScoreChanged?.Invoke(Score);
-    }
+	public void AddScore(int value)
+	{
+		Score += value;
+		ScoreChanged?.Invoke(Score);
+	}
 }
